0004c17e39ee2e05840d58423dc3bfeb9679adcd
James LeDoux <ledouxja@bc.edu>
2017-06-22 22:44:44 -0400

    add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pybaseball',
+        version='0.1',
+        description='retrieve baseball data in python',
+        author='James LeDoux',
+        author_email='ledoux.james.r@gmail.com',
+        url='github.com/jldbc/pybaseball',
+        license='MIT',
+        packages=['pybaseball'],
+        zip_safe=False)
