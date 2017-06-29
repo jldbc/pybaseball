46b54a39aed578db0a88ec943de018a36f5050b8
James LeDoux <ledouxja@bc.edu>
2017-06-28 22:56:17 -0400

    Made imports easier

--- pybaseball/__init__.py
+++ pybaseball/__init__.py
@@ -1,1 +1,6 @@
 from .statcast import get_statcast
+from .league_batting_stats import batting_stats
+from .league_batting_stats import batting_stats_range
+from .league_pitching_stats import pitching_stats
+from .league_pitching_stats import pitching_stats_range
+from .standings import standings
