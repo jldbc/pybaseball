99b36166116a48a9a06b7f3bc8f901abcba60b84
James LeDoux <ledouxja@bc.edu>
2017-07-10 22:11:43 -0400

    Im an idiot. this works now.

--- pybaseball/statcast.py
+++ pybaseball/statcast.py
@@ -17,16 +17,13 @@ def sanitize_input(start_dt, end_dt):
 		today = datetime.datetime.today()
 		start_dt = (today - datetime.timedelta(1)).strftime("%Y-%m-%d")
 		end_dt = today.strftime("%Y-%m-%d")
-
 		print("Warning: no date range supplied. Returning yesterday's Statcast data. For a different date range, try get_statcast(start_dt, end_dt).")
-
 	#if only one date is supplied, assume they only want that day's stats
 	#query in this case is from date 1 to date 1
 	if start_dt is None:
 		start_dt = end_dt
 	if end_dt is None:
 		end_dt = start_dt
-
 	# now that both dates are not None, make sure they are valid date strings
 	validate_datestring(start_dt)
 	validate_datestring(end_dt)
@@ -41,13 +38,8 @@ def statcast(start_dt=None, end_dt=None, team=None):
 
 	# inputs are valid if either both or zero dates are supplied. Not valid of only one given.
 	if start_dt and end_dt:
-		url = "https://baseballsavant.mlb.com/statcast_search/csv?all=true&hfPT=&hfAB=&hfBBT=&hfPR\
-			=&hfZ=&stadium=&hfBBL=&hfNewZones=&hfGT=R%7CPO%7CS%7C=&hfSea=&hfSit=&player_type=\
-			pitcher&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt={}&\
-			game_date_lt={}&team=&position=&hfRO=&home_road=&hfFlag=&metric_1=&hfInn=&min_pitches\
-			=0&min_results=0&group_by=name&sort_col=pitches&player_event_sort=h_launch_speed&\
-			sort_order=desc&min_abs=0&type=details&".format(start_dt, end_dt)
-		s=requests.get(url).content
+		url = "https://baseballsavant.mlb.com/statcast_search/csv?all=true&hfPT=&hfAB=&hfBBT=&hfPR=&hfZ=&stadium=&hfBBL=&hfNewZones=&hfGT=R%7CPO%7CS%7C=&hfSea=&hfSit=&player_type=pitcher&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt={}&game_date_lt={}&team=&position=&hfRO=&home_road=&hfFlag=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name&sort_col=pitches&player_event_sort=h_launch_speed&sort_order=desc&min_abs=0&type=details&".format(start_dt, end_dt)
+		s=requests.get(url, timeout=None).content
 		df = pd.read_csv(io.StringIO(s.decode('utf-8')))
 
 		#select only pitches from a particular team
