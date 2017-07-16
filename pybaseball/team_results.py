d11fddea1f84c346f176ef2710b98dbd3aba03e3
James LeDoux <ledouxja@bc.edu>
2017-07-16 13:28:07 -0400

    make attendance numbers usable, squashed some bugs relating to missing data in older seasons

--- pybaseball/team_results.py
+++ pybaseball/team_results.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
@@ -17,8 +18,11 @@ def get_soup(season, team):
 	s=requests.get(url).content
 	return BeautifulSoup(s, "html.parser")
 
-def get_table(soup):
-	table = soup.find_all('table')[0]
+def get_table(soup,team):
+	try:
+		table = soup.find_all('table')[0]
+	except:
+		raise ValueError("Data cannot be retrieved for this team/year combo. Please verify that your team abbreviation is accurate and that the team existed during the season you are searching for.")
 	data = []
 	headings = [th.get_text() for th in table.find("tr").find_all("th")]
 	headings = headings[1:] # the "gm#" heading doesn't have a <td> element
@@ -31,6 +35,8 @@ def get_table(soup):
 		try:
 			cols = row.find_all('td')
 			#links = row.find_all('a')
+			if cols[1].text == "":
+				cols[1].string = team # some of the older season don't seem to have team abbreviation on their tables
 			if cols[3].text == "":
 				cols[3].string = 'Home' # this element only has an entry if it's an away game
 			if cols[12].text == "":
@@ -41,6 +47,13 @@ def get_table(soup):
 				cols[14].string = "None" # games w/o saves have blank td entry
 			if cols[8].text == "":
 				cols[8].string = "9" # entry is blank if no extra innings
+			if cols[16].text=="":
+				cols[16].string = "Unknown"
+			if cols[15].text=="":
+				cols[15].string = "Unknown"
+			if cols[17].text == "": 
+				cols[17].string = "Unknown" # Unknown if attendance data is missing
+
 			cols = [ele.text.strip() for ele in cols]
 			data.append([ele for ele in cols if ele])
 		except:
@@ -54,21 +67,39 @@ def get_table(soup):
 	data = data.rename(columns=data.iloc[0])
 	data = data.reindex(data.index.drop(0))
 	data = data.drop('',1) #not a useful column
+	data['Attendance'].replace(r'^Unknown$', np.nan, regex=True, inplace = True) # make this a NaN so the column can benumeric
 	return data
 
 def process_win_streak(data):
 	"""
 	Convert "+++"/"---" formatted win/loss streak column into a +/- integer column
 	"""
-	data['Streak2'] = data['Streak'].str.len()
-	data.loc[data['Streak'].str[0]=='-','Streak2'] = -data['Streak2']
-	data['Streak'] = data['Streak2']
-	data = data.drop('Streak2',1)
+	#only do this if there are non-NANs in the column
+	if data['Streak'].count()>0:
+		data['Streak2'] = data['Streak'].str.len()
+		data.loc[data['Streak'].str[0]=='-','Streak2'] = -data['Streak2']
+		data['Streak'] = data['Streak2']
+		data = data.drop('Streak2',1)
+	return data
+
+def make_numeric(data):
+	# first remove commas from attendance values
+	# skip if column is all NA (not sure if everyone kept records in the early days)
+	if data['Attendance'].count()>0:
+		data['Attendance'] = data['Attendance'].str.replace(',','')
+		#data[num_cols] = data[num_cols].astype(float)
+	else:
+		data['Attendance'] = np.nan
+
+	# now make everything numeric
+	num_cols = ["R","RA","Inn","Rank","Attendance"]#,'Streak']
+	data[num_cols] = data[num_cols].astype(float) #not int because of NaNs
 	return data
 
 def schedule_and_record(season=None, team=None):
 	# retrieve html from baseball reference
 	soup = get_soup(season, team)
-	table = get_table(soup)
+	table = get_table(soup,team)
 	table = process_win_streak(table)
+	table = make_numeric(table)
 	return table
