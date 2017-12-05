34222207602631cbc172fcf21a0aaef7cb5ad1d4
James Ledoux <ledouxja@bc.edu>
2017-12-04 23:17:46 -0800

    Add functions for downloading all of lahman and just parks

--- pybaseball/lahman.py
+++ pybaseball/lahman.py
@@ -8,11 +8,34 @@
 
 import requests
 import zipfile
-from io import BytesIO
+import pandas as pd
+from io import BytesIO, StringIO
 from bs4 import BeautifulSoup
 
-# Download zip file and extract all files into working directory
 url = "http://seanlahman.com/files/database/baseballdatabank-2017.1.zip"
-s=requests.get(url,stream=True)
-z = zipfile.ZipFile(BytesIO(s.content))
-z.extractall()
+base_string = "baseballdatabank-2017.1/core/"
+
+def get_lahman_zip():
+	# Retrieve the Lahman database zip file. Making this a function since everything else
+	# will be re-using these lines
+	s=requests.get(url,stream=True)
+	z = zipfile.ZipFile(BytesIO(s.content))
+	return z
+
+def download_lahman():
+	# download entire lahman db to present working directory
+	z = get_lahman_zip()
+	z.extractall()
+
+def lahman_parks():
+	# do this for every table in the lahman db so they can exist as separate functions
+	z = get_lahman_zip()
+	f = base_string + "Parks.csv"
+	data = pd.read_csv(z.open(f), header=0, sep=',', quotechar="'")
+	return data
+
+
+#df = lahman_parks()
+#print(df.head())
+#download_lahman()
+
