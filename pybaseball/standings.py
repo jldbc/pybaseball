3f29cced707f3ba04cee6fbf3b0054325c83275c
kunalcsc630 <32287094+kunalcsc630@users.noreply.github.com>
2018-02-11 20:04:27 -0500

    Extend capability of standings.py (#14)
    * Extend capability of standings.py
    
    Added ability to pull standings up to from 1903 instead of up to 1969. Working on extending ability all the way to 1871.
    
    * Fix local variable reference
    
    Trying to fix the error here: https://github.com/jldbc/pybaseball/pull/14
    
    * Fix typo
    
    * Extended access to standings from 1871 to present
    
    * Fix logical error

--- pybaseball/standings.py
+++ pybaseball/standings.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from bs4 import Comment
 import requests
 import datetime
 import pandas as pd 
@@ -8,17 +9,45 @@ def get_soup(year):
     s=requests.get(url).content
     return BeautifulSoup(s, "html.parser")
 
-def get_tables(soup):
-    tables = soup.find_all('table')
+def get_tables(soup, season):
     datasets = []
-    for table in tables:
+    if(season>=1969):
+        tables = soup.find_all('table')
+        for table in tables:
+            data = []
+            headings = [th.get_text() for th in table.find("tr").find_all("th")]
+            data.append(headings)
+            table_body = table.find('tbody')
+            rows = table_body.find_all('tr')
+            for row in rows:
+                cols = row.find_all('td')
+                cols = [ele.text.strip() for ele in cols]
+                cols.insert(0,row.find_all('a')[0]['title']) # team name
+                data.append([ele for ele in cols if ele])
+            datasets.append(data)
+    else:
         data = []
+        table = soup.find('table')
         headings = [th.get_text() for th in table.find("tr").find_all("th")]
+        headings[0] = "Name"
+        if(season>=1930):
+            for i in range(15): headings.pop()
+        elif(season>=1876): 
+            for i in range(14): headings.pop()
+        else:
+            for i in range(16): headings.pop()
         data.append(headings)
         table_body = table.find('tbody')
         rows = table_body.find_all('tr')
         for row in rows:
+            if row.find_all('a') == []: continue
             cols = row.find_all('td')
+            if(season>=1930):
+                for i in range(15): cols.pop()
+            elif(season>=1876): 
+                for i in range(14): cols.pop()
+            else:
+                for i in range(16): cols.pop()
             cols = [ele.text.strip() for ele in cols]
             cols.insert(0,row.find_all('a')[0]['title']) # team name
             data.append([ele for ele in cols if ele])
@@ -32,11 +61,18 @@ def standings(season=None):
     # get most recent standings if date not specified
     if(season is None):
         season = int(datetime.datetime.today().strftime("%Y"))
-    if season<1969:
-        raise ValueError("This query currently only returns division standings, which did not exist until the 1969 season. Try looking at years from 1969 to present.")
+    if season<1871:
+        raise ValueError("This query currently only returns standings until the 1871 season. Try looking at years from 1871 to present.")
     # retrieve html from baseball reference
     soup = get_soup(season)
-    tables = get_tables(soup)
+    if season>=1969:
+        tables = get_tables(soup, season)
+    else:
+        t = soup.find_all(string=lambda text:isinstance(text,Comment))
+        for i, c in enumerate(t):
+            if i==16 and season<1903: code = BeautifulSoup(c, "html.parser")
+            elif i==17 and season>=1903: code = BeautifulSoup(c, "html.parser")
+        tables = get_tables(code, season)
     tables = [pd.DataFrame(table) for table in tables]
     for idx in range(len(tables)):
         tables[idx] = tables[idx].rename(columns=tables[idx].iloc[0])
