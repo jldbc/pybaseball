bb8348a26717c85babb12ca39bf0d1b72940a545
kunalcsc630 <32287094+kunalcsc630@users.noreply.github.com>
2018-02-22 17:12:53 -0500

    Extend capability of playerid_lookup.py
    Allows users to make spelling mistakes in their searches

--- pybaseball/playerid_lookup.py
+++ pybaseball/playerid_lookup.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import requests
 import io
+import difflib
 
 # dropped key_uuid. looks like a has we wouldn't need for anything. 
 # TODO: allow for typos. String similarity? 
@@ -31,10 +32,33 @@ def playerid_lookup(last, first=None):
     if first:
         first = first.lower()
     table = get_lookup_table()
+    results = []
     if first is None:
         results = table.loc[table['name_last']==last]
+        if results.empty:
+            print("Player not found. Add a first name to your search for more accurate results.")
     else:
         results = table.loc[(table['name_last']==last) & (table['name_first']==first)]
+        if results.empty:
+            print("Player not found. Gathering a list of alternative players.")
+            suggestions = []
+            first_name = SequenceMatcher()
+            first_name.set_seq1(first)
+            last_name = SequenceMatcher()
+            last_name.set_seq1(last)
+            for f, l in zip(table['name_first'], table['name_last']):
+                # elements in table are floats so need to be converted to str when setting to sequence matcher
+                first_name.set_seq2(str(f))
+                last_name.set_seq2(str(l))
+                if first_name.ratio() >= 0.6 and last_name.ratio() >= 0.6: # 0.6 comes from suggested value: https://docs.python.org/3/library/difflib.html
+                    if [l,f] in suggestions: continue
+                    else: suggestions.append([l,f])
+            for i, p in enumerate(suggestions):
+                print(i, p, end='\n')
+            print("If you meant to search one of these players, enter the number to the left of the player. \nOtherwise, type '-1' to end the method. If multiple players have the same name, all will \nbe shown for the user to choose.")
+            pos = int(input())
+            if pos == -1: print("Ending the method."); return
+            else: results = table.loc[(table['name_last']==suggestions[pos][0]) & (table['name_first']==suggestions[pos][1])]
     #results[['key_mlbam', 'key_fangraphs', 'mlb_played_first', 'mlb_played_last']] = results[['key_mlbam', 'key_fangraphs', 'mlb_played_first', 'mlb_played_last']].astype(int) # originally returned as floats which is wrong
     results = results.reset_index().drop('index', 1)
     return results
