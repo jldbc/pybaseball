6d0952c51bc3f00a4ff54b6a137a0b82eafe2d75
James LeDoux <jledoux@jamess-macbook.lan>
2018-04-22 22:54:53 -0700

    fix statcast player id filtering bug

--- pybaseball/statcast_batter.py
+++ pybaseball/statcast_batter.py
@@ -16,23 +16,19 @@ def sanitize_input(start_dt, end_dt, batter_id):
         raise ValueError("Player ID is required. If you need to find a player's id, try pybaseball.playerid_lookup(last_name, first_name) and use their key_mlbam. If you want statcast data for all players, try the statcast() function.")
     # this id should be a string to place inside a url
     batter_id = str(batter_id)
-
     # if no dates are supplied, assume they want yesterday's data
     # send a warning in case they wanted to specify
     if start_dt is None and end_dt is None:
         today = datetime.datetime.today()
         start_dt = (today - datetime.timedelta(1)).strftime("%Y-%m-%d")
         end_dt = today.strftime("%Y-%m-%d")
-
         print("Warning: no date range supplied. Returning yesterday's Statcast data. For a different date range, try get_statcast(start_dt, end_dt).")
-
     #if only one date is supplied, assume they only want that day's stats
     #query in this case is from date 1 to date 1
     if start_dt is None:
         start_dt = end_dt
     if end_dt is None:
         end_dt = start_dt
-
     # now that both dates are not None, make sure they are valid date strings
     validate_datestring(start_dt)
     validate_datestring(end_dt)
@@ -48,10 +44,9 @@ def statcast_batter(start_dt=None, end_dt=None, player_id=None):
     player_id : INT : the player's MLBAM ID. Find this by calling pybaseball.playerid_lookup(last_name, first_name), finding the correct player, and selecting their key_mlbam. 
     """
     start_dt, end_dt, player_id = sanitize_input(start_dt, end_dt, player_id)
-
     # inputs are valid if either both or zero dates are supplied. Not valid of only one given.
     if start_dt and end_dt:
-        url = 'https://baseballsavant.mlb.com/statcast_search/csv?all=true&hfPT=&hfAB=&hfBBT=&hfPR=&hfZ=&stadium=&hfBBL=&hfNewZones=&hfGT=R%7CPO%7CS%7C=&hfSea=&hfSit=&player_type=batter&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt={}&game_date_lt={}&player_lookup%5B%5D={}&team=&position=&hfRO=&home_road=&hfFlag=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name&sort_col=pitches&player_event_sort=h_launch_speed&sort_order=desc&min_abs=0&type=details&'.format(start_dt,end_dt,player_id)
+        url = 'https://baseballsavant.mlb.com/statcast_search/csv?all=true&hfPT=&hfAB=&hfBBT=&hfPR=&hfZ=&stadium=&hfBBL=&hfNewZones=&hfGT=R%7CPO%7CS%7C=&hfSea=&hfSit=&player_type=batter&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt={}&game_date_lt={}&batters_lookup%5B%5D={}&team=&position=&hfRO=&home_road=&hfFlag=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name&sort_col=pitches&player_event_sort=h_launch_speed&sort_order=desc&min_abs=0&type=details&'.format(start_dt,end_dt,player_id)
         s=requests.get(url).content
         df = pd.read_csv(io.StringIO(s.decode('utf-8')))
         return df
--- pybaseball/statcast_pitcher.py
+++ pybaseball/statcast_pitcher.py
@@ -49,7 +49,7 @@ def statcast_pitcher(start_dt=None, end_dt=None, player_id=None):
 
     # inputs are valid if either both or zero dates are supplied. Not valid of only one given.
     if start_dt and end_dt:
-        url = 'https://baseballsavant.mlb.com/statcast_search/csv?all=true&hfPT=&hfAB=&hfBBT=&hfPR=&hfZ=&stadium=&hfBBL=&hfNewZones=&hfGT=R%7CPO%7CS%7C=&hfSea=&hfSit=&player_type=pitcher&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt={}&game_date_lt={}&player_lookup%5B%5D={}&team=&position=&hfRO=&home_road=&hfFlag=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name&sort_col=pitches&player_event_sort=h_launch_speed&sort_order=desc&min_abs=0&type=details&'.format(start_dt,end_dt,player_id)
+        url = 'https://baseballsavant.mlb.com/statcast_search/csv?all=true&hfPT=&hfAB=&hfBBT=&hfPR=&hfZ=&stadium=&hfBBL=&hfNewZones=&hfGT=R%7CPO%7CS%7C=&hfSea=&hfSit=&player_type=pitcher&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt={}&game_date_lt={}&pitchers_lookup%5B%5D={}&team=&position=&hfRO=&home_road=&hfFlag=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name&sort_col=pitches&player_event_sort=h_launch_speed&sort_order=desc&min_abs=0&type=details&'.format(start_dt,end_dt,player_id)
 
         s=requests.get(url).content
         df = pd.read_csv(io.StringIO(s.decode('utf-8')))
