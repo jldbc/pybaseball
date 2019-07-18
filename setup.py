03d5a85dfa4f18b004d7d25e70bc6e50ca9bb646
James LeDoux <ledouxja@bc.edu>
2019-07-17 22:08:56 -0400

    Version bump 1.0.8 (#62)
    * version bump
    
    * specify content type for package description

--- setup.py
+++ setup.py
@@ -29,11 +29,11 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.5',
+    version='1.0.8',
 
     description='Retrieve baseball data in Python',
     long_description=long_description,
-
+    long_description_content_type="text/markdown",
     # The project's main homepage.
     url='https://github.com/jldbc/pybaseball',
 
