009985ec2f247618b9279fc48a596c5d1beaf977
Adam Weeden <adamweeden@gmail.com>
2020-09-02 17:29:47 -0400

    Add docstrings

--- tests/pybaseball/conftest.py
+++ tests/pybaseball/conftest.py
@@ -9,12 +9,25 @@ from _pytest.monkeypatch import MonkeyPatch
 
 @pytest.fixture()
 def data_dir() -> str:
+    """
+        Returns the path to the tests data directory
+    """
     this_dir = os.path.dirname(os.path.realpath(__file__))
     return os.path.join(this_dir, 'data')
 
 @pytest.fixture()
 def get_data_file_contents(data_dir: str) -> Callable:
+    """
+        Returns a function that will allow getting the contents of a file in the tests data directory easily
+    """
     def get_contents(filename: str) -> str:
+        """
+            Get the str contents of a file in the tests data directory
+
+                
+            ARGUMENTS:
+            filename    : str : the name of the file within the tests data directory to get the contents of
+        """
         with open(os.path.join(data_dir, filename)) as _file:
             return _file.read()
     
@@ -22,16 +35,39 @@ def get_data_file_contents(data_dir: str) -> Callable:
 
 @pytest.fixture()
 def get_data_file_dataframe(data_dir: str) -> Callable:
+    """
+        Returns a function that will allow getting a dataframe from a csv file in the tests data directory easily
+    """
     def get_dataframe(filename: str) -> pd.DataFrame:
+        """
+            Get the DatFrame representation of the contents of a csv file in the tests data directory
+
+                
+            ARGUMENTS:
+            filename    : str : the name of the file within the tests data directory to load into a DataFrame
+        """
         return pd.read_csv(os.path.join(data_dir, filename), index_col=0).reset_index(drop=True)
 
     return get_dataframe
 
 @pytest.fixture()
 def response_get_monkeypatch(monkeypatch: MonkeyPatch) -> Callable:
-    def setup(result: str, expected_url: str):
+    """
+        Returns a function that will monkeypatch the requests.get function call to return expected data 
+    """
+    def setup(result: str, expected_url: str = None):
+        """
+           Get the DatFrame representation of the contents of a csv file in the tests data directory
+
+                
+            ARGUMENTS:
+            result          : str            : the payload to return in the contents of the request.get call
+            expected_url    : str (optional) : an expected_url to test the get call against
+                                               to ensure the correct endpoint is hit
+        """
         def _monkeypatch(url: str) -> object:
-            assert url.endswith(expected_url)
+            if expected_url is not None:
+                assert url.endswith(expected_url)
 
             class DummyResponse:
                 def __init__(self, html: str):
