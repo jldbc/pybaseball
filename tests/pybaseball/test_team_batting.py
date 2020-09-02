c75ad49256a2eab45d04d047e09f46fd23904fd8
Adam Weeden <adamweeden@gmail.com>
2020-09-02 00:03:41 -0400

    Move common test code to fixtures

--- tests/pybaseball/test_team_batting.py
+++ tests/pybaseball/test_team_batting.py
@@ -1,6 +1,3 @@
-import os
-
-import numpy as np
 import pandas as pd
 import pytest
 import requests
@@ -9,21 +6,13 @@ from pybaseball.team_batting import team_batting, _FG_TEAM_BATTING_URL
 
 
 @pytest.fixture()
-def data_dir():
-    this_dir = os.path.dirname(os.path.realpath(__file__))
-    return os.path.join(this_dir, 'data')
+def sample_html(get_data_file_contents):
+    return get_data_file_contents('team_batting.html')
 
 
 @pytest.fixture()
-def sample_html(data_dir):
-    with open(os.path.join(data_dir, 'team_batting.html')) as html:
-        return html.read()
-
-
-@pytest.fixture()
-def sample_processed_result(data_dir):
-    return pd.read_csv(os.path.join(data_dir, 'team_batting.csv'), index_col=0).reset_index(drop=True)
-
+def sample_processed_result(get_data_file_dataframe):
+    return get_data_file_dataframe('team_batting.csv')
 
 class TestTeamBatting:
     def test_team_batting(self, monkeypatch, sample_html, sample_processed_result):
