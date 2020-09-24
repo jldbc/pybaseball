be4a2cf99061dc557a73b8465260c774b0d538c1
Adam Weeden <adamweeden@gmail.com>
2020-09-24 16:28:57 -0400

    This wrapper didn't even make sense...

--- pybaseball/lahman.py
+++ pybaseball/lahman.py
@@ -25,7 +25,6 @@ def get_lahman_zip() -> Optional[ZipFile]:
         _handle = ZipFile(BytesIO(s.content))
     return _handle
 
-@caching.dataframe_cache()
 def download_lahman():
     # download entire lahman db to present working directory
     z = get_lahman_zip()
