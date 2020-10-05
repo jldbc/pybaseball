e663e51e9673a7259afe8e532b682b87e4eaad18
Adam Weeden <adamweeden@gmail.com>
2020-10-05 12:06:56 -0400

    Fix some formatting

--- tests/pybaseball/test_amateur_draft.py
+++ tests/pybaseball/test_amateur_draft.py
@@ -3,27 +3,29 @@ from typing import Callable
 import pandas as pd
 import pytest
 
-from pybaseball.amateur_draft import amateur_draft, _URL
+from pybaseball.amateur_draft import _URL, amateur_draft
+
+from .conftest import GetDataFrameCallable
 
 
 @pytest.fixture(name="sample_html")
-def _sample_html(get_data_file_contents: Callable) -> str:
+def _sample_html(get_data_file_contents: Callable[[str], str]) -> str:
     return get_data_file_contents('amateur_draft.html')
 
 
 @pytest.fixture(name="sample_processed_result")
-def _sample_processed_result(get_data_file_dataframe: Callable) -> pd.DataFrame:
+def _sample_processed_result(get_data_file_dataframe: GetDataFrameCallable) -> pd.DataFrame:
     return get_data_file_dataframe('amateur_draft_keep_stats.csv')
 
 
 @pytest.fixture(name="sample_processed_result_no_stats")
-def _sample_processed_result_no_stats(get_data_file_dataframe: Callable) -> pd.DataFrame:
+def _sample_processed_result_no_stats(get_data_file_dataframe: GetDataFrameCallable) -> pd.DataFrame:
     return get_data_file_dataframe('amateur_draft_no_stats.csv')
 
 
 def test_amateur_draft(response_get_monkeypatch: Callable, sample_html: str,
                        sample_processed_result: pd.DataFrame) -> None:
-    expected_url = _URL.format(year=2019, round=1)
+    expected_url = _URL.format(year=2019, draft_round=1)
 
     response_get_monkeypatch(sample_html, expected_url)
     result = amateur_draft(2019, 1)
@@ -35,8 +37,8 @@ def test_amateur_draft(response_get_monkeypatch: Callable, sample_html: str,
 
 
 def test_amateur_draft_no_stats(response_get_monkeypatch: Callable, sample_html: str,
-                       sample_processed_result_no_stats: pd.DataFrame) -> None:
-    expected_url = _URL.format(year=2019, round=1)
+                                sample_processed_result_no_stats: pd.DataFrame) -> None:
+    expected_url = _URL.format(year=2019, draft_round=1)
 
     response_get_monkeypatch(sample_html, expected_url)
     result = amateur_draft(2019, 1, keep_stats=False)
