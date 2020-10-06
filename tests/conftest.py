3a3bf35b210649680100ac9e2ef2040bcf9fccfc
Adam Weeden <adamweeden@gmail.com>
2020-10-06 11:26:03 -0400

    Remove pylint comment config

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,7 +14,7 @@ from pybaseball import cache
 def _logging_side_effect() -> Callable:
     def _logger(name: str, after: Optional[Callable] = None) -> Callable:
         def _side_effect(*args: Any, **kwargs: Any) -> Optional[Any]:
-            logging.debug(f'Mock {name} => {args} {kwargs}')  # pylint: disable=logging-fstring-interpolation
+            logging.debug(f'Mock {name} => {args} {kwargs}')
             if after is not None:
                 return after(*args, **kwargs)
 
