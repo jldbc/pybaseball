dff9b10ff0bcfd0bd82bb6019d91cb683918ceab
Ben <ben.dilday.phd@gmail.com>
2020-10-20 07:08:19 -0400

    adds argparser and cleans up print string

--- scripts/statcast_timing.py
+++ scripts/statcast_timing.py
@@ -1,27 +1,30 @@
+import argparse
 import sys
 import time
 from pybaseball import statcast
 
-EXPECTED_TIME_THRESHOLD = 30
-START_DATE = "2018-08-01"
-END_DATE = "2018-08-10"
+DEFAULT_TIME_THRESHOLD = 30
+DEFAULT_START_DATE = "2018-08-01"
+DEFAULT_END_DATE = "2018-08-10"
+
+
+def _parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--start-date", required=False, default=DEFAULT_START_DATE)
+    parser.add_argument("--end-date", required=False, default=DEFAULT_END_DATE)
+    parser.add_argument("--time-threshold", "-t", required=False, type=float, default=DEFAULT_TIME_THRESHOLD)
+    return parser.parse_args()
+
 
 def main():
-    if len(sys.argv) == 2:
-        time_threshold = float(sys.argv[1])
-    else:
-        time_threshold = EXPECTED_TIME_THRESHOLD
-
+    args = _parse_args()
     start_time = time.time()
-    _ = statcast(START_DATE, END_DATE)
+    _ = statcast(args.start_date, args.end_date)
     end_time = time.time()
     query_time = end_time-start_time
-    diagnostic_messages = f"query took {query_time: .1f} seconds which is", f"threshold of {time_threshold: .1f}\n"
-    if query_time > time_threshold:
-        sys.stderr.write("{0} greater than {1}".format(*diagnostic_messages))
-        sys.exit(1)
-    else:
-        sys.stdout.write("{0} less than {1}".format(*diagnostic_messages))
+    threshold_exceeded = query_time > args.time_threshold
+    print(f"query took {query_time: .1f} (expected less than {args.time_threshold: .1f})")
+    sys.exit(int(threshold_exceeded))
 
 if __name__=='__main__':
     main()
