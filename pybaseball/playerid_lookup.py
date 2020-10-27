5310828159880000f103a828d4218311569183a2
Moshe Schorr <mosheraphael@gmail.com>
2020-10-27 15:41:18 +0200

    Change to a client-based API

--- pybaseball/playerid_lookup.py
+++ pybaseball/playerid_lookup.py
@@ -1,15 +1,18 @@
 import io
 import os
 
+from typing import List
+
 import pandas as pd
 import requests
+from fuzzywuzzy import process
 
 from . import cache
-from fuzzywuzzy import process
 
 # dropped key_uuid. looks like a has we wouldn't need for anything.
 
 url = "https://raw.githubusercontent.com/chadwickbureau/register/master/data/people.csv"
+
 
 def get_register_file():
     return os.path.join(cache.config.cache_directory, 'chadwick-register.csv')
@@ -29,7 +32,7 @@ def chadwick_register(save: bool = False) -> pd.DataFrame:
     cols_to_keep = ['name_last', 'name_first', 'key_mlbam'] + mlb_only_cols
     table = pd.read_csv(io.StringIO(s.decode('utf-8')), usecols=cols_to_keep)
 
-    table.dropna(how='all', subset=mlb_only_cols, inplace=True) # Keep only the major league rows
+    table.dropna(how='all', subset=mlb_only_cols, inplace=True)  # Keep only the major league rows
     table.reset_index(inplace=True, drop=True)
 
     table[['key_mlbam', 'key_fangraphs']] = table[['key_mlbam', 'key_fangraphs']].fillna(-1)
@@ -47,10 +50,11 @@ def chadwick_register(save: bool = False) -> pd.DataFrame:
 
 def get_lookup_table(save=False):
     table = chadwick_register(save)
-    #make these lowercase to avoid capitalization mistakes when searching
+    # make these lowercase to avoid capitalization mistakes when searching
     table['name_last'] = table['name_last'].str.lower()
     table['name_first'] = table['name_first'].str.lower()
     return table
+
 
 def name_similarity(last: str, first: str, player_table: pd.DataFrame) -> pd.DataFrame:
     """Calculates similarity of first and last name provided with all players in player_table
@@ -67,92 +71,98 @@ def name_similarity(last: str, first: str, player_table: pd.DataFrame) -> pd.Dat
     chadwick_names = filled_df["name_first"] + " " + filled_df["name_last"]
     fuzzy_matches = pd.DataFrame(process.extract(f"{first} {last}", chadwick_names, limit=5))
     matched_names = fuzzy_matches[0].str.split(expand=True)
-    matched_names = matched_names.rename(columns={0:"name_first", 1:"name_last"})
+    matched_names = matched_names.rename(columns={0: "name_first", 1: "name_last"})
     return matched_names
-    
-def playerid_lookup(last: str = None, first: str = None, player_list: list = None, search: bool = False) -> pd.DataFrame:
-    """Lookup playerIDs (MLB AM, bbref, retrosheet, FG) for a given player
 
-    Args:
-        last (str, optional): Player's last name. Defaults to None.
-        first (str, optional): Player's first name. Defaults to None.
-        player_list (list, optional): List of players to seach for (comma delimited). Defaults to None.
-        search (bool, optional): In case of typos, returns players with names close to input. Defaults to False.
 
-    Returns:
-        pd.DataFrame: DataFrame of playerIDs, name, years played
-    """
+class player_search_client:
+    def __init__(self) -> None:
+        self.table = get_lookup_table()
 
-    # force input strings to lowercase
-    if last:
-        last = last.lower()
-    if first:
-        first = first.lower()
-    table = get_lookup_table()
+    def search(self, last: str = None, first: str = None, player_list: List[str] = None, fuzzy: bool = False) -> pd.DataFrame:
+        """Lookup playerIDs (MLB AM, bbref, retrosheet, FG) for a given player
 
-    # if player_list has a value, then the user is passing in a list of players
-    # the list of players may be comma delimited for last, first, or just last
-    if player_list:
-        player_counter = 1
-        for player in player_list:
-            last = player.split(",")[0].strip()
-            first = None
-            if (len(player.split(",")) > 1):
-                first = player.split(",")[1].strip()
-            if (player_counter == 1):
-                results = playerid_lookup(last, first)
-            else:
-                results = results.append(playerid_lookup(last, first), ignore_index=True)
-            player_counter += 1
+        Args:
+            last (str, optional): Player's last name. Defaults to None.
+            first (str, optional): Player's first name. Defaults to None.
+            player_list (list, optional): List of players to seach for (comma delimited). Defaults to None.
+            search (bool, optional): In case of typos, returns players with names close to input. Defaults to False.
+
+        Returns:
+            pd.DataFrame: DataFrame of playerIDs, name, years played
+        """
+
+        # force input strings to lowercase
+        if last:
+            last = last.lower()
+        if first:
+            first = first.lower()
+
+        # if player_list has a value, then the user is passing in a list of players
+        # the list of players may be comma delimited for last, first, or just last
+        if player_list:
+            player_counter = 1
+            for player in player_list:
+                last = player.split(",")[0].strip()
+                first = None
+                if (len(player.split(",")) > 1):
+                    first = player.split(",")[1].strip()
+                if (player_counter == 1):
+                    results = self.search(last, first)
+                else:
+                    results = results.append(self.search(last, first), ignore_index=True)
+                player_counter += 1
+            return results
+
+        if first is None:
+            results = self.table.loc[self.table['name_last'] == last]
+        else:
+            results = self.table.loc[(self.table['name_last'] == last) & (self.table['name_first'] == first)]
+
+        results = results.reset_index(drop=True)
+
+        # If no matches, return 5 closest names
+        if len(results) == 0 and fuzzy:
+            print("No identically matched names found! Returning the 5 most similar names.")
+            similar_names_df = name_similarity(last=last, first=first, player_table=self.table)
+            results = similar_names_df.merge(self.table, left_on=["name_last", "name_first"], right_on=[
+                                             "name_last", "name_first"], how="left")
+
         return results
 
-    if first is None:
-        results = table.loc[table['name_last'] == last]
-    else:
-        results = table.loc[(table['name_last'] == last) & (table['name_first'] == first)]
+    def reverse_lookup(self, player_ids: List[str], key_type=None) -> pd.DataFrame:
+        """Retrieve a table of player information given a list of player ids
 
-    results = results.reset_index(drop=True)
+        :param player_ids: list of player ids
+        :type player_ids: list
+        :param key_type: name of the key type being looked up (one of "mlbam", "retro", "bbref", or "fangraphs")
+        :type key_type: str
 
-    # If no matches, return 5 closest names
-    if len(results) == 0 and search:
-        print("No identically matched names found! Returning the 5 most similar names.")
-        similar_names_df = name_similarity(last=last, first=first, player_table=table)
-        results = similar_names_df.merge(table, left_on=["name_last", "name_first"], right_on=["name_last","name_first"], how="left")
+        :rtype: :class:`pandas.core.frame.DataFrame`
+        """
+        key_types = (
+            'mlbam',
+            'retro',
+            'bbref',
+            'fangraphs',
+        )
 
-    return results
+        if not key_type:
+            key_type = key_types[0]  # default is "mlbam" if key_type not provided
+        elif key_type not in key_types:
+            raise ValueError('[Key Type: {}] Invalid; Key Type must be one of "{}"'.format(
+                key_type, '", "'.join(key_types)))
 
+        key = 'key_{}'.format(key_type)
 
-# data = playerid_lookup('bonilla')
-# data = playerid_lookup('bonilla', 'bobby')
+        results = self.table[self.table[key].isin(player_ids)]
+        results = results.reset_index(drop=True)
 
+        return results
 
-def playerid_reverse_lookup(player_ids, key_type=None):
-    """Retrieve a table of player information given a list of player ids
+# These errors will be removed in a later version
+def playerid_lookup(*args, **kwargs) -> None:
+    raise RuntimeError("This function has been removed in favor of the player_search_client usage")
 
-    :param player_ids: list of player ids
-    :type player_ids: list
-    :param key_type: name of the key type being looked up (one of "mlbam", "retro", "bbref", or "fangraphs")
-    :type key_type: str
-
-    :rtype: :class:`pandas.core.frame.DataFrame`
-    """
-    key_types = (
-        'mlbam',
-        'retro',
-        'bbref',
-        'fangraphs',
-    )
-
-    if not key_type:
-        key_type = key_types[0]  # default is "mlbam" if key_type not provided
-    elif key_type not in key_types:
-        raise ValueError('[Key Type: {}] Invalid; Key Type must be one of "{}"'.format(
-            key_type, '", "'.join(key_types)))
-
-    table = get_lookup_table()
-    key = 'key_{}'.format(key_type)
-
-    results = table[table[key].isin(player_ids)]
-    results = results.reset_index(drop=True)
-
-    return results
+def playerid_reverse_lookup(*args, **kwargs) -> None:
+    raise RuntimeError("This function has been removed in favor of the player_search_client usage")
--- tests/integration/pybaseball/test_playerid_lookup.py
+++ tests/integration/pybaseball/test_playerid_lookup.py
@@ -1,33 +1,38 @@
 import pandas as pd
 
-from pybaseball.playerid_lookup import playerid_lookup
+from pybaseball.playerid_lookup import player_search_client
 
 
 def test_playerid_lookup_misspelling() -> None:
     """Test simple 'hank aaron' misspelling"""
-    aaron_df = playerid_lookup("aron", "hak", search=True)
+    client = player_search_client()
+    aaron_df = client.search("aron", "hak", fuzzy=True)
     assert aaron_df["name_first"][0] == "hank"
     assert aaron_df["name_last"][0] == "aaron"
 
 def test_playerid_lookup_multiple_player() -> None:
     """Test multiple players per name - two pedro martinez"""
-    assert len(playerid_lookup("martinez", "pedro", search=True)) == 2
+    client = player_search_client()
+    assert len(client.search("martinez", "pedro", fuzzy=True)) == 2
 
 def test_playerid_lookup_last_name() -> None:
     """Test last name-only search"""
-    gariciappara_df = playerid_lookup("gariciappara", search=True)
+    client = player_search_client()
+    gariciappara_df = client.search("gariciappara", fuzzy=True)
     assert gariciappara_df["name_first"][0] == "nomar"
     assert gariciappara_df["name_last"][0] == "garciaparra"
 
 def test_playerid_lookup_phonetic_yastrzemski() -> None:
     """Test phonetically tougher names with reasonable guesses (use-case)"""
     # Mike Yastrezremski
-    yastrzemski_df = playerid_lookup("yastremsky", "mike", search=True)
+    client = player_search_client()
+    yastrzemski_df = client.search("yastremsky", "mike", fuzzy=True)
     assert yastrzemski_df["name_last"][0] == "yastrzemski"
 
 def test_playerid_lookup_phonetic_bogaerts() -> None:
     """Test phonetically tougher names with reasonable guesses (use-case)"""
     # Xander Bogaerts
-    bogaerts_df = playerid_lookup("bogarts", "zander", search=True)
+    client = player_search_client()
+    bogaerts_df = client.search("bogarts", "zander", fuzzy=True)
     assert bogaerts_df["name_last"][0] == "bogaerts"
     assert bogaerts_df["name_first"][0] == "xander"
