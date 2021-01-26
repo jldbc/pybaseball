482ddd966d37cc209c00b3c1728f69560e8aa6b8
Adam Weeden <adamweeden@gmail.com>
2021-01-26 11:23:44 -0500

    Why did statcast data change? ¯\_(ツ)_/¯

--- tests/integration/pybaseball/test_statcast_batter.py
+++ tests/integration/pybaseball/test_statcast_batter.py
@@ -10,7 +10,8 @@ def test_statcast_batter_exitvelo_barrels() -> None:
     assert not result.empty
 
     assert len(result.columns) == 19
-    assert len(result) == 250
+    assert len(result) == 135
+
 
 def test_statcast_batter() -> None:
     result: pd.DataFrame = statcast_batter('2019-01-01', '2019-12-31', 642715)
