1b8bb709acd17cbc073bfdf5e6f6671068e128b5
Ford Higgins <wfordh@users.noreply.github.com>
2021-02-17 12:27:59 -0800

    Statcast batter leaderboards (#179)
    * Additional Statcast Batter Leaderboards
    
    * add pitch arsenal leaderboard
    
    * docs for new statcast batter functions
    
    * Make exitvelo test a little more resilient (hopefully for the last time) - w/ Adam Weeden

--- pybaseball/statcast_batter.py
+++ pybaseball/statcast_batter.py
@@ -1,5 +1,5 @@
 import io
-from typing import Optional
+from typing import Optional, Union
 
 import pandas as pd
 import requests
@@ -29,9 +29,30 @@ def statcast_batter(start_dt: Optional[str] = None, end_dt: Optional[str] = None
     return df
 
 @cache.df_cache()
-def statcast_batter_exitvelo_barrels(year: int, minBBE: str = "q") -> pd.DataFrame:
+def statcast_batter_exitvelo_barrels(year: int, minBBE: Union[int, str] = "q") -> pd.DataFrame:
     url = f"https://baseballsavant.mlb.com/leaderboard/statcast?type=batter&year={year}&position=&team=&min={minBBE}&csv=true"
     res = requests.get(url, timeout=None).content
     data = pd.read_csv(io.StringIO(res.decode('utf-8')))
+    return data
 
+@cache.df_cache()
+def statcast_batter_expected_stats(year: int, minPA: Union[int, str] = "q") -> pd.DataFrame:
+    url = f"https://baseballsavant.mlb.com/leaderboard/expected_statistics?type=batter&year={year}&position=&team=&min={minPA}&csv=true"
+    res = requests.get(url, timeout=None).content
+    data = pd.read_csv(io.StringIO(res.decode('utf-8')))
+    return data
+
+@cache.df_cache()
+def statcast_batter_percentile_ranks(year: int) -> pd.DataFrame:
+    url = f"https://baseballsavant.mlb.com/leaderboard/percentile-rankings?type=batter&year={year}&position=&team=&csv=true"
+    res = requests.get(url, timeout=None).content
+    data = pd.read_csv(io.StringIO(res.decode('utf-8')))
+    # URL returns a null player with player id 999999, which we want to drop
+    return data.loc[data.player_name.notna()].reset_index(drop=True)
+
+@cache.df_cache()
+def statcast_batter_pitch_arsenal(year: int, minPA: int = 25) -> pd.DataFrame:
+    url = f"https://baseballsavant.mlb.com/leaderboard/pitch-arsenal-stats?type=batter&pitchType=&year={year}&team=&min={minPA}&csv=true"
+    res = requests.get(url, timeout=None).content
+    data = pd.read_csv(io.StringIO(res.decode('utf-8')))
     return data
