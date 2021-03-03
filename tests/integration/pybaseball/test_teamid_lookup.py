dccc804ac4992f0e596856ec31de2d4d4ec083fe
Adam Weeden <adamweeden@gmail.com>
2021-03-03 15:22:11 -0500

    Add integration test to ensure no cross matches occur

--- tests/integration/pybaseball/test_teamid_lookup.py
+++ tests/integration/pybaseball/test_teamid_lookup.py
@@ -16,6 +16,17 @@ def test_generate_fangraphs_teams() -> None:
         result = result.query('yearID <= 2019')
 
         assert len(result.columns) == 7
-        assert len(result) == 2925
 
         to_csv_mock.assert_called_once_with(_DATA_FILENAME)
+
+        # Each franchID should match one and only one teamIDfg
+        for franch_id in set(result['franchID'].values):
+            teams = result.query(f"franchID == '{franch_id}'")
+            franchises = teams.groupby("teamIDfg").size()
+            if len(franchises.index) > 1:
+                # Print it out so we know who the offender is
+                print('franch_id', franch_id)
+                print('franchises', franchises)
+                print(teams)
+            
+            assert len(franchises.index) == 1
