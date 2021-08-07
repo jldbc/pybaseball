ca2d3e39be1b2581f050771e6f096e174433e85e
prs-watch <scarsdaleny15@yahoo.co.jp>
2021-08-07 20:40:47 +0900

    ~fix revert formatting

--- pybaseball/__init__.py
+++ pybaseball/__init__.py
@@ -70,11 +70,6 @@ from .retrosheet import events
 from .team_game_logs import team_game_logs
 from .plotting import plot_stadium
 from .plotting import spraychart
-from .datasources.fangraphs import (
-    fg_batting_data,
-    fg_pitching_data,
-    fg_team_batting_data,
-    fg_team_fielding_data,
-    fg_team_pitching_data,
-)
+from .datasources.fangraphs import (fg_batting_data, fg_pitching_data, fg_team_batting_data, fg_team_fielding_data,
+                                    fg_team_pitching_data)
 from .split_stats import get_splits
