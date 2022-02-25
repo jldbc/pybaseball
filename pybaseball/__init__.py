495d3190750e78a37200dea09ef03b643513ee04
Michael Fagan <michael.g.fagan@gmail.com>
2022-02-25 17:18:24 -0600

    Update lahman for changes to directory structure

--- pybaseball/__init__.py
+++ pybaseball/__init__.py
@@ -76,7 +76,8 @@ from .lahman import pitching_post
 from .lahman import salaries
 from .lahman import schools
 from .lahman import series_post
-from .lahman import teams
+from .lahman import teams_core
+from .lahman import teams_upstream
 from .lahman import teams_franchises
 from .lahman import teams_half
 from .lahman import download_lahman
