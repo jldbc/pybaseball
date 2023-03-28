3ccd099a80e374e45c8e5597ea06b0fdc3ae651c
Erin McNulty <eem2188@columbia.edu>
2023-03-28 09:03:06 -0400

    Added docstrings to any undocumented public-facing functions (#340)
    * Added docstrings to any undocuments public-facing functions
    
    * responded to initial round of feedback

--- pybaseball/statcast_batter.py
+++ pybaseball/statcast_batter.py
@@ -13,9 +13,10 @@ def statcast_batter(start_dt: Optional[str] = None, end_dt: Optional[str] = None
     Pulls statcast pitch-level data from Baseball Savant for a given batter.
 
     ARGUMENTS
-    start_dt : YYYY-MM-DD : the first date for which you want a player's statcast data
-    end_dt : YYYY-MM-DD : the final date for which you want data
-    player_id : INT : the player's MLBAM ID. Find this by calling pybaseball.playerid_lookup(last_name, first_name), finding the correct player, and selecting their key_mlbam.
+        start_dt : YYYY-MM-DD : the first date for which you want a player's statcast data
+        end_dt : YYYY-MM-DD : the final date for which you want data
+        player_id : INT : the player's MLBAM ID. Find this by calling pybaseball.playerid_lookup(last_name, first_name), 
+            finding the correct player, and selecting their key_mlbam.
     """
     start_dt, end_dt, _ = sanitize_input(start_dt, end_dt, player_id)
     
@@ -30,6 +31,15 @@ def statcast_batter(start_dt: Optional[str] = None, end_dt: Optional[str] = None
 
 @cache.df_cache()
 def statcast_batter_exitvelo_barrels(year: int, minBBE: Union[int, str] = "q") -> pd.DataFrame:
+    """
+    Retrieves batted ball data for all batters in a given year.
+
+    ARGUMENTS
+        year: The year for which you wish to retrieve batted ball data. Format: YYYY.
+        minBBE: The minimum number of batted ball events for each player. If a player falls 
+            below this threshold, they will be excluded from the results. If no value is specified, 
+            only qualified batters will be returned.
+    """
     url = f"https://baseballsavant.mlb.com/leaderboard/statcast?type=batter&year={year}&position=&team=&min={minBBE}&csv=true"
     res = requests.get(url, timeout=None).content
     data = pd.read_csv(io.StringIO(res.decode('utf-8')))
@@ -38,6 +48,14 @@ def statcast_batter_exitvelo_barrels(year: int, minBBE: Union[int, str] = "q") -
 
 @cache.df_cache()
 def statcast_batter_expected_stats(year: int, minPA: Union[int, str] = "q") -> pd.DataFrame:
+    """
+    Retrieves expected stats based on quality of batted ball contact in a given year.
+
+    ARGUMENTS
+        year: The year for which you wish to retrieve expected stats data. Format: YYYY.
+        minPA: The minimum number of plate appearances for each player. If a player falls below this threshold, 
+            they will be excluded from the results. If no value is specified, only qualified batters will be returned.
+    """
     url = f"https://baseballsavant.mlb.com/leaderboard/expected_statistics?type=batter&year={year}&position=&team=&min={minPA}&csv=true"
     res = requests.get(url, timeout=None).content
     data = pd.read_csv(io.StringIO(res.decode('utf-8')))
@@ -46,6 +64,13 @@ def statcast_batter_expected_stats(year: int, minPA: Union[int, str] = "q") -> p
 
 @cache.df_cache()
 def statcast_batter_percentile_ranks(year: int) -> pd.DataFrame:
+    """
+    Retrieves percentile ranks for each player in a given year, including batters with at least 2.1 PA per team 
+    game and 1.25 for pitchers.
+
+    ARGUMENTS
+        year: The year for which you wish to retrieve percentile data. Format: YYYY.
+    """
     url = f"https://baseballsavant.mlb.com/leaderboard/percentile-rankings?type=batter&year={year}&position=&team=&csv=true"
     res = requests.get(url, timeout=None).content
     data = pd.read_csv(io.StringIO(res.decode('utf-8')))
@@ -54,6 +79,14 @@ def statcast_batter_percentile_ranks(year: int) -> pd.DataFrame:
 
 @cache.df_cache()
 def statcast_batter_pitch_arsenal(year: int, minPA: int = 25) -> pd.DataFrame:
+    """
+    Retrieves outcome data for batters split by the pitch type in a given year.
+
+    ARGUMENTS
+        year: The year for which you wish to retrieve pitch arsenal data. Format: YYYY.
+        minPA: The minimum number of plate appearances for each player. If a player falls below this threshold, 
+            they will be excluded from the results. If no value is specified, the default number of plate appearances is 25.
+    """
     url = f"https://baseballsavant.mlb.com/leaderboard/pitch-arsenal-stats?type=batter&pitchType=&year={year}&team=&min={minPA}&csv=true"
     res = requests.get(url, timeout=None).content
     data = pd.read_csv(io.StringIO(res.decode('utf-8')))
