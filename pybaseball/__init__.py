ac0003210c26426e809609c2a36188b524a93cd0
James Maslek <jmaslek11@gmail.com>
2024-06-18 11:12:13 -0400

    Redirect the lahman database to lahman's  dropbox site

--- pybaseball/__init__.py
+++ pybaseball/__init__.py
@@ -79,7 +79,7 @@ from .lahman import salaries
 from .lahman import schools
 from .lahman import series_post
 from .lahman import teams_core
-from .lahman import teams_upstream
+# from .lahman import teams_upstream  # Not in the Lahman database
 from .lahman import teams_franchises
 from .lahman import teams_half
 from .lahman import download_lahman
