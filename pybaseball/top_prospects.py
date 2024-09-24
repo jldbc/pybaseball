aa093f9d52054b8b7ec5988deb2a8dbb7fcb0f89
Ben Dilday <bdilday@users.noreply.github.com>
2024-09-24 14:57:45 -0400

    pass user agent to get request (#448)

--- pybaseball/top_prospects.py
+++ pybaseball/top_prospects.py
@@ -22,8 +22,16 @@ def top_prospects(teamName=None, playerType=None):
     else:
         mlbTeamId = teamid_lookup.mlb_team_id(teamName)
         url = f"https://www.mlb.com/prospects/stats?teamId={mlbTeamId}"
-    
-    res = requests.get(url, timeout=None).content
+
+    res = requests.get(
+        url,
+        timeout=None,
+        headers={
+            "UserAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) C"
+            "hrome/104.0.5112.79 Safari/537.36"
+        },
+    ).content
     prospectList = pd.read_html(res)
     
     if playerType == "batters":
